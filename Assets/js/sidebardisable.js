2efb732ef22ed62af147f20b5be412f6bf463cb3
naraagst <naraagst12@gmail.com>
2020-07-24 12:07:44 +0800

    sidebar edit

--- Assets/js/sidebardisable.js
+++ Assets/js/sidebardisable.js
@@ -22,39 +22,39 @@ const getShowClass = (path) => {
 document.querySelector(".sidebar").innerHTML = `
 			<ul>
 				<li><a href="../../../../index.html"><i class="fas fa-home" style="font-size:25px;color:white;margin-right:10px;margin-top: -14px;"></i>Beranda</a></li>
-				<li class="dropdown1${getChapClass("A")}">
+				<li class="dropdown1${getChapClass("a")}">
 					<a href=javascript:void(0) class="dropbtn" id="materiA" onclick="dropdown6()" style="height:50px;">
 						<div class="teksmenu">A. Menganalisis Data</div>
 						<div class="icon"><i class="fas fa-angle-down" style="font-size:25px;color:white;margin-right:10px;margin-top: -14px;"></i></div>
 					</a>
-					<div class="dropdown ${getShowClass("A")}" id="myDropdown6">
+					<div class="dropdown ${getShowClass("a")}" id="myDropdown6">
 					  <a href="../../A/analisis1.html" class="${getNavLinkClass("/Assets/materi/A/analisis1.html")}">Materi</a>
-				      <a href="../../A/evaluasiA" class="${getChapClasses("A")}">Kuis</a>
+				      <a href="../../A/evaluasiA" class="${getChapClasses("a")}">Kuis</a>
 				    </div>
 				</li>
-				<li class="dropdown1 ${getChapClass("B")}">
+				<li class="dropdown1 ${getChapClass("b")}">
 					<a href=javascript:void(0) class="dropbtn" id="materiB" onclick="dropdown()" style="height:70px;">
 						<div class="teksmenu">B. Ukuran Pemusatan Data </div>
 						<div class="icon"><i class="fas fa-angle-down" style="font-size:25px;color:white;margin-right:10px;margin-top: -14px;"></i></div>
 					</a>
-					<div class="dropdown ${getShowClass("B")}" id="myDropdown">
+					<div class="dropdown ${getShowClass("b")}" id="myDropdown">
 				      <a href="../../B/mean1.html" class="${getNavLinkClass("/Assets/materi/B/mean1.html")}">1.  Mean</a>
 				      <a href="../../B/median1.html" class="${getNavLinkClass("/Assets/materi/B/median1.html")}">2.  Median</a>
 				      <a href="../../B/modus1.html" class="${getNavLinkClass("/Assets/materi/B/modus1.html")}">3.  Modus</a>
-				      <a href="../../B/evaluasiB" class="${getChapClasses("B")}">Kuis</a>
+				      <a href="../../B/evaluasiB" class="${getChapClasses("b")}">Kuis</a>
 				    </div>
 				</li>
-				<li class="dropdown1 ${getChapClass("C")}">
+				<li class="dropdown1 ${getChapClass("c")}">
 					<a href=javascript:void(0) class="dropbtn" id="materiC" onclick="dropdown2()" style="height:70px;">
 						<div class="teksmenu">C. Ukuran Penyebaran Data</div>
 						<div class="icon"><i class="fas fa-angle-down" style="font-size:25px;color:white;margin-right:10px;margin-top: -14px;"></i></div>
 					</a>
-					<div class="dropdown ${getShowClass("C")}" id="myDropdown2">
+					<div class="dropdown ${getShowClass("c")}" id="myDropdown2">
 				      <a href="../../C/jangkauan1.html" class="${getNavLinkClass("/Assets/materi/C/jangkauan1.html")}">1. Jangkauan</a>
 				      <a href="../../C/kuartil1.html" class="${getNavLinkClass("/Assets/materi/C/kuartil1.html")}">2. Kuartil</a>
 				      <a href="../../C/interkuartil1.html" class="${getNavLinkClass("/Assets/materi/C/interkuartil1.html")}">3. Jangkauan Interkuartil</a>
 				      <a href="../../C/simpangan1.html" class="${getNavLinkClass("/Assets/materi/C/simpangan1.html")}">4. Simpangan Kuartil</a>
-				      <a href="../../C/evaluasiC" class="${getChapClasses("C")}">Kuis</a>
+				      <a href="../../C/evaluasiC" class="${getChapClasses("c")}">Kuis</a>
 				    </div>
 				</li>
 				<li><a href="../../Latihan" class="${getLatClass("l")}" id="latihan"><i class="fas fa-edit" style="font-size:21px;color:white;margin-right:10px;margin-top: -14px;"></i>Latihan</a>
