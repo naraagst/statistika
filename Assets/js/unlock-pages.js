0fbfacfa4fa9ebf7a4802f78888fd2626858dff2
naraagst <naraagst12@gmail.com>
2020-08-24 12:15:06 +0800

    edit index kuis

--- Assets/js/unlock-pages.js
+++ Assets/js/unlock-pages.js
@@ -1,15 +1,15 @@
-const materiA = document.querySelector("#materiA");
-const materiB = document.querySelector("#materiB");
-const materiC = document.querySelector("#materiC");
+// const materiA = document.querySelector("#materiA");
+// const materiB = document.querySelector("#materiB");
+// const materiC = document.querySelector("#materiC");
 
-let getPage = (idName, localName) => {
-  const getLocal = localStorage.getItem(localName);
-  // console.log(idName);
-  if (getLocal != null) {
-      idName.classList.remove("disabled");
-  }
-};
+// let getPage = (idName, localName) => {
+//   const getLocal = localStorage.getItem(localName);
+//   // console.log(idName);
+//   if (getLocal != null) {
+//       idName.classList.remove("disabled");
+//   }
+// };
 
-getPage(materiA, "materiA");
-getPage(materiB, "materiB");
-getPage(materiC, "materiC");
+// getPage(materiA, "materiA");
+// getPage(materiB, "materiB");
+// getPage(materiC, "materiC");
